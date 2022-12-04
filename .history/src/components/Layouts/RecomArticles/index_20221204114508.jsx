d2dec5817f54eb5fedbdb20af2a1b7368132c6d1
asahi <ojimaasahi1018@gmail.com>
2022-12-04 13:16:13 +0900

    微調整

---
+++ .history/src/components/Layouts/RecomArticles/index_20221204114508.jsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from "react";
+import ArticleDate from "src/components/elements/ArticleDate";
+import ArticleImg from "src/components/elements/ArticleImg";
+import LoctionPoint from "src/components/elements/LocationPoint";
+import SectionWraper from "src/components/elements/SectionWrapper";
+import { useTabletMqContext } from "src/Context/MediaQueryContext";
+
+import {
+  ArticleContentdiv,
+  ArticlesWraper,
+  ArticleWraper,
+  Title,
+} from "./style";
+
+export default function RecomArticles({ recomBlogs }) {
+  const isTablet = useTabletMqContext();
+  return (
+    <SectionWraper type={"recomArti"}>
+      <h2>おすすめの記事</h2>
+      <ArticlesWraper>
+        {recomBlogs.map((_blog) => {
+          return (
+            <ArticleWraper key={_blog.id}>
+              <ArticleImg
+                src={_blog.main_img.url}
+                alt="メインビジュアル"
+                id={_blog.id}
+              />
+              <ArticleContentdiv>
+                <Title>
+                  <Link href={`/blog/${_blog.id}`}>{_blog.title}</Link>
+                </Title>
+                {isTablet && (
+                  <>
+                    <LoctionPoint areaList={_blog.areas} />
+                    <ArticleDate utcDate={_blog.publishedAt} />
+                  </>
+                )}
+              </ArticleContentdiv>
+            </ArticleWraper>
+          );
+        })}
+      </ArticlesWraper>
+    </SectionWraper>
+  );
+}
---
+++ .history/src/components/elements/Toc/index_20221204115613.jsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+import {
+  ListItem,
+  ListTilte,
+  Title,
+  TocBtn,
+  TocContents,
+  TocSection,
+} from "./style";
+
+export default function Toc({ toc }) {
+  const [isSeeMore, setIsSeeMore] = useState(false);
+  const newToc = [
+    ...toc.map((item) => {
+      if (item.name === "H2") {
+        return (
+          <ListTilte key={item.id}>
+            <Link href={`#${item.id}`}>
+              <a>{item.text}</a>
+            </Link>
+          </ListTilte>
+        );
+      } else {
+        return (
+          <ListItem key={item.id}>
+            <Link href={`#${item.id}`}>
+              <a>{item.text}</a>
+            </Link>
+          </ListItem>
+        );
+      }
+    }),
+  ];
+  function TocHander(aryLength) {
+    const _ary = [];
+    for (let i = 0; i < aryLength; i++) {
+      _ary.push(newToc[i]);
+    }
+    return _ary;
+  }
+  const tocList = TocHander(isSeeMore ? newToc.length : 6);
+  return (
+    <>
+      {Boolean(newToc.length) && (
+        <TocSection>
+          <TocContents>{tocList}</TocContents>
+          {tocList.length <= 6 && !isSeeMore && (
+            <TocBtn onClick={() => setIsSeeMore((prev) => !prev)}>
+              もっとみる
+            </TocBtn>
+          )}
+        </TocSection>
+      )}
+    </>
+  );
+}
